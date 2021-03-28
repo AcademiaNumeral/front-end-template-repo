e64cf3192ab0a3447d65f38491a0e7eb46dc2ea4
Manuel Silva <manuel.silvag1@gmail.com>
2021-03-28 18:40:36 -0500

    gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,22 @@ const autoprefixer = require('gulp-autoprefixer');
 const del = require('del');
 const { series, watch } = require('gulp');
 
+const distFolder = (process.env.NODE_ENV === 'ghpages') ? 'docs' : 'dist'
+
 function compileSass() {
   return gulp.src(['src/scss/globals.scss'])
     .pipe(sass())
     .pipe(autoprefixer({
         cascade: false
     }))
-    .pipe(gulp.dest("dist/css"))
+    .pipe(gulp.dest(`${distFolder}/css`))
     .pipe(browserSync.stream());
 }
 
 function compilePug() {
   return gulp.src(['src/pug/*.pug'])
     .pipe(pug())
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest(distFolder))
     .pipe(browserSync.stream());
 }
 
@@ -30,36 +32,36 @@ function transpileJs() {
     .pipe(babel({
       presets: ['@babel/env']
     }))
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest(`${distFolder}/js`))
     .pipe(browserSync.stream());;
 }
 
 function minifyImages() {
   return gulp.src(['src/assets/img/*'])
     .pipe(imagemin())
-    .pipe(gulp.dest("dist/assets/img"))
+    .pipe(gulp.dest(`${distFolder}/assets/img`))
     .pipe(browserSync.stream());
 }
 
 function moveHtml() {
   return gulp.src(['src/*.html'])
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest(distFolder))
 }
 
 function loadIcons() {
   return gulp.src(['src/assets/icons/*'])
-    .pipe(gulp.dest("dist/assets/icons"))
+    .pipe(gulp.dest(`${distFolder}/assets/icons`))
 }
 
 function cleanDist() {
   return del([
-    'dist/*'
+    `${distFolder}/*`, `!${distFolder}/fonts`
   ])
 }
 
 function serve() {
   browserSync.init({
-    server: "./dist"
+    server: `./${distFolder}`
   });
   watch('src/js/**/*.js', {usePolling: true}, transpileJs);
   watch('src/scss/**/*.scss', {usePolling: true}, compileSass);
@@ -67,10 +69,10 @@ function serve() {
   watch(['src/assets/img/**/*.svg', 'src/assets/img/**/*.jpg', 'src/assets/img/**/*.png'], {usePolling: true}, minifyImages);
   watch(['src/assets/icons/**/*.ico', 'src/assets/icons/**/*.png'], {usePolling: true}, loadIcons);
   watch('src/*.html', moveHtml);
-  watch("dist/*.html",{usePolling: true},).on('change', browserSync.reload);
+  watch(`${distFolder}/*.html`,{usePolling: true},).on('change', browserSync.reload);
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'ghpages') {
   exports.default = series(cleanDist, transpileJs, compileSass, compilePug, minifyImages, moveHtml, loadIcons);
 } else {
   exports.default = series(cleanDist, transpileJs, compileSass, compilePug, minifyImages, moveHtml, loadIcons, serve);
