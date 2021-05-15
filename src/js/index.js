7b4a72013495d0002667fecd5730cac3a293e9b3
Manuel Silva <manuel.silvag1@gmail.com>
2021-05-15 11:22:52 -0500

    Css loader, font loader y copy para archivos

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import { saveStudent } from './repo/studentsRepo.js';
+import '../scss/global.scss';
 
 const saveContactForm = () => {
   const contactForm = document.getElementById("contactForm");
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: './js/main.js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]',
   },
   resolve: {
     extensions: ['.js'],
@@ -18,6 +22,45 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff',
+            name: '[name].[ext]',
+            outputPath: './assets/fonts',
+            publicPath: './assets/fonts',
+            esModule: false,
+          }
+        }
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/index.html',
+      filename: './index.html',
+      scriptLoading: 'defer',
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets/img'),
+          to: 'assets/img',
+        }
+      ]
+    })
+  ]
 }
